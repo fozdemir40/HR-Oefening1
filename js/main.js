b4df4031de429142389506b7fe8b3e851404e9ef
Furkan Ozdemir <f.ozdemir40@hotmail.com>
2020-05-12 11:05:07 +0200

    init project

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//
+// vis element
+//
+let fish = document.getElementsByTagName("fish")[0]
+fish.style.transform = "translate(200px, 100px)"
+fish.style.filter = "hue-rotate(200deg)"
+
+//
+// bubble element
+//
+let bubble = document.getElementsByTagName("bubble")[0]
+bubble.style.transform = "translate(20vw, 100vh)"
